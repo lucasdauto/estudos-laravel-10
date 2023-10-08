97a72f02c21538a8262637db33e60a1d13a03cae
Lucas Dauto <lucasdauto@gmail.com>
2023-10-08 14:23:44 -0300

    start in route, controller and blade

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\SupportController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/supports', [SupportController::class, 'index']);
+
